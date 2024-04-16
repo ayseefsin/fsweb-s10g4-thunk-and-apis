a7f4e77b7609eb2124489adad588723fa684b677
ayseefsin <ayse.efsin@yandex.com>
2024-04-16 17:37:40 +0300

    flip style added

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ data }) {
+  const [showPunchline, setShowPunchline] = useState(false);
   return (
-    <div className="shadow-md bg-white text-center">
-      <p className="text-2xl p-10">{data.setup}</p>
+    <div className="flip shadow-md bg-white text-center">
+      <div className="flip-content">
+        <p className="flip-front text-2xl p-10">{data.setup}</p>
+        <p className=" flip-back text-2xl p-10">{data.punchline}</p>
+      </div>
     </div>
   );
 }
