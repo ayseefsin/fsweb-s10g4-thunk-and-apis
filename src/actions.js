86cc7a167fab981389e98a950115ab6d021769ee
ayseefsin <ayse.efsin@yandex.com>
2024-04-16 16:41:29 +0300

    initial fetch and get joke

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,6 @@
+import { type } from "@testing-library/user-event/dist/type";
+import axios from "axios";
+
 export const GET_FAVS_FROM_LS = "GET_FAVS_FROM_LS";
 export const FAV_ADD = "FAV_ADD";
 export const FAV_REMOVE = "FAV_REMOVE";
@@ -6,16 +9,26 @@ export const FETCH_LOADING = "FETCH_LOADING";
 export const FETCH_ERROR = "FETCH_ERROR";
 
 export const getFavsFromLocalStorage = () => {
-  return { type: GET_FAVS_FROM_LS }
-}
+  return { type: GET_FAVS_FROM_LS };
+};
 
 export const addFav = (info) => {
-  return { type: FAV_ADD, payload: info }
-}
+  return { type: FAV_ADD, payload: info };
+};
 
 export const removeFav = (id) => {
-  return { type: FAV_REMOVE, payload: id }
-}
+  return { type: FAV_REMOVE, payload: id };
+};
 
-export const fetchAnother = () => dispatch => {
-}
+export const fetchAnother = () => (dispatch) => {
+  axios
+    .get("https://official-joke-api.appspot.com/random_joke")
+    .then(function (response) {
+      console.log(response.data);
+      dispatch({ type: FETCH_SUCCESS, payload: response.data });
+    })
+    .catch(function (error) {
+      dispatch({ type: FETCH_ERROR, payload: error.message });
+    })
+    .finally(function () {});
+};
