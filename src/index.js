8d83186f48b5340d3716ee263fb99bb003b33469
ayseefsin <ayse.efsin@yandex.com>
2024-04-16 14:09:26 +0300

    setup tools redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { myReducer } from './reducers';
+import { myReducer } from "./reducers";
+import { thunk } from "redux-thunk";
+import { applyMiddleware } from "redux";
+import logger from "redux-logger";
 
-const depo = createStore(myReducer);
+const depo = createStore(myReducer, applyMiddleware(thunk, logger));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={depo}>
     <BrowserRouter>
